5c479a3e4dc584b7529af5d64505b270ec86b7ee
Sanyok <89658217860s@gmail.com>
2023-12-28 11:46:36 +0700

    start

---
+++ app/src/main/java/com/example/scherbakov_v_3/personal_area.java
@@ -0,0 +1,13 @@
+package com.example.scherbakov_v_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class personal_area extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_area);
+    }
+}
